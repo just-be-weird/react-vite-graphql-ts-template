ce566ff7f645dd7451ac19bdf0194049b1d13223
just-be-weird <17427511+just-be-weird@users.noreply.github.com>
2023-10-13 14:35:58 +0530

    Updates the query

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,27 +1,2 @@
 import { gql } from '@apollo/client';
-
-export const GET_ALL_FILMS = gql`
-  query AllFilms {
-    allFilms {
-      totalCount
-      edges {
-        node {
-          episodeID
-          id
-          title
-          planetConnection {
-            edges {
-              node {
-                id
-                name
-              }
-              cursor
-            }
-            totalCount
-          }
-        }
-        cursor
-      }
-    }
-  }
-`;
+export const GET_ALL_CHARACTERS = gql``;
