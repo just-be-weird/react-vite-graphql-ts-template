57c6771b662cf8353b647d718460f89b2bce265b
just-be-weird <17427511+just-be-weird@users.noreply.github.com>
2023-10-25 17:28:33 +0530

    Removes yarn and uses node

--- src/api/hook/useDataStore.ts
+++ src/api/hook/useDataStore.ts
@@ -1,47 +1,13 @@
 import { gql, useQuery } from '@apollo/client';
 
-interface Episode {
-  id: string;
-  name: string;
-}
+interface Episode {}
 
-interface Character {
-  id: string;
-  image: string;
-  name: string;
-}
+interface Character {}
 
-type EpisodeData = {
-  results: Episode[];
-};
-type EpisodeResponse = {
-  episodes: EpisodeData;
-};
-type CharacterResponse = {
-  episode: {
-    characters: Character[];
-  };
-};
+type EpisodeData = {};
+type EpisodeResponse = {};
+type CharacterResponse = {};
 
 export const useDataStore = () => {
-  const {
-    data,
-    loading,
-  }: { data: EpisodeResponse | undefined; loading: boolean } = useQuery(gql`
-    query Episodes($page: Int, $filter: FilterEpisode) {
-      episodes(page: $page, filter: $filter) {
-        results {
-          id
-          name
-          characters {
-            id
-            name
-            image
-          }
-        }
-      }
-    }
-  `);
-
-  return { data, loading };
+  return { data: {}, loading: false };
 };
