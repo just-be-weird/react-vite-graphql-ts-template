86e0dd1b25baec1c7799b778d59938d9c5aabf3a
just-be-weird <17427511+just-be-weird@users.noreply.github.com>
2024-09-16 15:24:05 +0530

    Updates problem

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,17 @@ import { Group } from '@mantine/core';
 import classes from './header.module.css';
 import { Welcome } from '@/components/Welcome';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle';
+import { LiftSystem } from '@/components/LiftSystem';
+import { Fragment } from 'react';
 
 export function Header() {
   return (
-    <Group className={classes.header} justify='space-between' w='100vw'>
-      <Welcome />
-      <ColorSchemeToggle />
-    </Group>
+    <Fragment>
+      <Group className={classes.header} justify='space-between' w='100vw'>
+        <Welcome />
+        <ColorSchemeToggle />
+      </Group>
+      <LiftSystem />
+    </Fragment>
   );
 }
